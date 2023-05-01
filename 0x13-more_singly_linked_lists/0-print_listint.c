d5315f408304e5813cf13d199790afbd84173ae7
Idodoo <dodoo.ivan@yahoo.com>
2023-05-01 19:52:42 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
-size_t print_listint(const listint *head)
+size_t print_listint(const listint *h)
 {
     size_t nnodes = 0;
-    while(head != NULL)
+    while(h != NULL)
     {
         printf("%d\n", head->n);
-        head = head->next;
+        h = h->next;
         nnodes++;
     }
     return  (nnodes);
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
