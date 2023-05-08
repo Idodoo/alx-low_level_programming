e58f15eb635413709c9fbc885629d38b424eda0f
Idodoo <dodoo.ivan@yahoo.com>
2023-05-08 07:57:37 +0000

    0-binary_to_uint

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,17 +4,14 @@
 *prints the binary representation of a number
 */
 
+void print_binary(unsigned long int n){
+    if(n >> 0){
+        if(n >> 1)
+        print_binary(n >> 1);
+        _putchar((n & 1) + '0');   
+    }
+    else {
+        _putchar('0')
+    }
 
-void print_binary(unsigned long int n)
-{
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
-	{
-		_putchar('0');
-	}
 }
