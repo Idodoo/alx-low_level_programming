737db5d16488275127a71cb50b2d7a2be5140876
Idodoo <dodoo.ivan@yahoo.com>
2023-05-09 01:28:30 +0000

    read_textfile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 /* Headers */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t_read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
