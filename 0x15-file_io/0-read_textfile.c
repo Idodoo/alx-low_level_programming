08a0b771041e0f38616d70bc245fa13997133552
Idodoo <dodoo.ivan@yahoo.com>
2023-05-13 11:55:38 +0000

    create_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,40 @@
 
 /**
  * read_textfile - read a text file and prints it
+ *
  * @filename: a pointer to the file to be a read
+ *
  * @letters: numbers of letters printed
+ *
  * Return: number of letters it read and printed otherwise return 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t fs;
-    char *a;
-    ssize_t r,w;
-    if(!filename)
-        return (0);
-    a = malloc(sizeof(char) * (letters));
-    if(a == NULL)
-        return (0);
-    fs = open(filename, O_RDONLY);
-    if (fs == -1)
-        return (0);
-    r = read(fs, a, letters);
-    w = write(STDOUT_FILENO, a, r);
+	char *a;
+	ssize_t fd;
+	ssize_t r;
+	ssize_t w;
 
-    close(a);
-    free(a);
-    return(w);
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	
+	if (fd == -1)
+	        return (0);
+		
+	
+	a = malloc(sizeof(char) * (letters));
+
+	if (!a)
+		return (0);
+	
+	r = read(fd, a, letters);
+	w = write(STDOUT_FILENO, a, r);
+
+	free(a);
+	close(fd);
+	return (w);
 
 }
