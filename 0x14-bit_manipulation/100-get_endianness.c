6de9cd9f2656fe34becc0f264f0cf4f5b63504ef
Idodoo <dodoo.ivan@yahoo.com>
2023-05-20 18:02:54 +0000

    refactor

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,7 +3,9 @@
 int get_endianness(void){
     unsigned int b;
     char *n;
+    /* Create a test integer and interpret its memory as a character pointer */
     b = 1;
     n = (char *) &b;
+     /* Return the value of the first byte to determine the endianness */
     return ((int)*n);
 }
