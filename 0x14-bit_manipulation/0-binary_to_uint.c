0dbb7c95b27be647e2508f01d3d86a56601bb197
Idodoo <dodoo.ivan@yahoo.com>
2023-07-07 00:10:52 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,23 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, res = 0, len = 0;
+	unsigned int base = 1, riz = 0, lon = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len])
-		len++;
+	while (b[lon])
+		lon++;
 
-	while (len)
+	while (lon)
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
+		if (b[lon - 1] != '0' && b[lon - 1] != '1')
 			return (0);
 
-		if (b[len - 1] == '1')
-			res += base;
+		if (b[lon - 1] == '1')
+			riz += base;
 		base *= 2;
-		len--;
+		lon--;
 	}
-	return (res);
+	return (riz);
 }
